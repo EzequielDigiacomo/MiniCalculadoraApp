2eab241a09ccd30c7019222cc419d07d41e5cbf3
EzequielDigiacomo <ezequiel250891@gmail.com>
2023-10-11 11:37:17 -0300

    Realizada, faltan styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'MiniCalculadoraApp';
+  title = 'Mini CalculadoraApp';
+  
+  numero1 : number = 0;
+  //segundo ejemplo bien hecho
+  numero2 : number = 0;
+  resultado : number = 0;
+
+  sumar(): void {
+
+    this.resultado = this.numero1 + this.numero2
+
+  }
+
+  restar(): void {
+
+    this.resultado = this.numero1 - this.numero2
+
+  }
+
+  multiplicar(): void {
+
+    this.resultado = this.numero1 * this.numero2
+
+  }
+
+  dividir(): void {
+
+    this.resultado = this.numero1 / this.numero2
+
+  }
+
+  resetear() : void{
+    this.resultado = 0;
+    this.numero1 = 0;
+    this.numero2 = 0;
+  }
+
 }
